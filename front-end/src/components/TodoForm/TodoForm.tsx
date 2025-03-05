b1ef15663387edc6cdd76e007e87b3582c53e1c2
petrdragonman <petrdragonman@me.com>
2025-03-05 22:08:05 +1100

    implements react hook form and zod

--- front-end/src/components/TodoForm/TodoForm.tsx
+++ front-end/src/components/TodoForm/TodoForm.tsx
@@ -1,40 +1,49 @@
-import { useRef } from "react";
+//import { useRef } from "react";
 import Button from "../Button/Button";
 import classes from "./TodoForm.module.scss";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { schema, TodoFormData } from "./schema";
 
 interface TodoFormProps {
-  onSubmit: (...args: any[]) => any;
+  //onSubmit: (...args: any[]) => any;
+  onSubmit: (data: TodoFormData) => unknown;
 }
 const TodoForm = ({ onSubmit }: TodoFormProps) => {
   const categories = ["study", "work", "hobby"];
-  const formRef = useRef<HTMLFormElement>(null);
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { isSubmitSuccessful, errors },
+  } = useForm<TodoFormData>({ resolver: zodResolver(schema) }); // TodoFormData
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const form = formRef.current;
-    if (!form) return;
-    const formData = new FormData(form);
-    const formValues = Object.fromEntries(formData.entries());
-    onSubmit(formValues);
-  };
+  isSubmitSuccessful && reset();
+
   return (
-    <form className={classes.form} onSubmit={handleSubmit} ref={formRef}>
+    <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
       <div className={classes.field}>
-        {/* <label htmlFor="todoInput">TODO</label> */}
         <input
           type="text"
           id="todoInput"
-          name="todo"
           placeholder="type new todo..."
+          {...register("todo")}
         />
+        {errors?.todo && (
+          <small className={classes.error_text}>{errors?.todo?.message}</small>
+        )}
       </div>
       <div className={classes.field}>
-        {/* <label htmlFor="categoryInput">CATEGORY</label> */}
-        <select id="categoryInput" name="category">
+        <select id="categoryInput" {...register("category")}>
           {categories.map((cat) => (
             <option key={cat}>{cat}</option>
           ))}
         </select>
+        {errors?.category && (
+          <small className={classes.error_text}>
+            {errors?.category?.message}
+          </small>
+        )}
       </div>
       <div className={classes.field}>
         <Button variant="PRIMARY" type="submit">
