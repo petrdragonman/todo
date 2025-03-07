6a1685f54ecb30cfe261822c7da42c79ce0634c1
petrdragonman <petrdragonman@me.com>
2025-03-08 08:51:40 +1100

    displays all available categories

--- src/main/java/com/petr/todo/HomeController.java
+++ src/main/java/com/petr/todo/HomeController.java
@@ -1,14 +1,14 @@
-package com.petr.todo;
+// package com.petr.todo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
-public class HomeController {
-    @GetMapping
-    public String hiFromPetr() {
-        return "Hi from petr!";
-    } 
+// @RestController
+// public class HomeController {
+//     @GetMapping
+//     public String hiFromPetr() {
+//         return "Hi from petr!";
+//     } 
     
-}
+// }
